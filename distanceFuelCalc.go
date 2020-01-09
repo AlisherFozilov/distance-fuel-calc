8fda33847215f644521631c04aad0a900c8c4308
Alisher <sambusa001@mail.ru>
2020-01-09 17:45:46 +0500

    fixed: #1

--- distanceFuelCalc.go
+++ distanceFuelCalc.go
@@ -4,8 +4,8 @@ func distanceFuelCalc(litersPerHundredKm float64, fuelInLiters float64) int {
 	const percent = 5
 
 	litersPerKm := litersPerHundredKm / 100.0
-	var distance int = int(fuelInLiters / litersPerKm)
-	var inaccuracy int = distance * percent / 100
+	distance := int(fuelInLiters / litersPerKm)
+	inaccuracy := distance * percent / 100
 	distance -= inaccuracy
 	return distance
 }
